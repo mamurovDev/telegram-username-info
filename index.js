c189c00a0849274c81b73bf864c3986744a66270
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-05-10 14:06:54 +0500

    handle base path

--- index.js
+++ index.js
@@ -63,3 +63,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.status(404).send("Page not found"); 
+});
